6b50670b6d7527239243062f4843a036d24f0da2
LMTD-Rom <LMTD-Rom@users.noreply.github.com>
2025-10-04 12:48:46 +0700

    Update social links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,9 +21,9 @@ const Footer = () => {
 
         {/* Ikon Sosmed - di tengah di mobile */}
         <div className="flex gap-3 order-2 md:order-none">
-          <a href="https://github.com/rissss21"><i className="ri-github-fill ri-2x"></i></a>
-          <a href="https://www.instagram.com/farisedrikprayoga/"><i className="ri-instagram-fill ri-2x"></i></a>
-          <a href="https://www.youtube.com/@FarisEdrikPrayoga"><i className="ri-youtube-fill ri-2x"></i></a>
+          <a href="https://github.com/LMTD-Rom"><i className="ri-github-fill ri-2x"></i></a>
+          <a href="https://www.instagram.com/khasbi_for.one?igsh=MWY4dzA1b3B2ejZ4OQ=="><i className="ri-instagram-fill ri-2x"></i></a>
+          <a href="https://www.youtube.com/@lmtd_maker"><i className="ri-youtube-fill ri-2x"></i></a>
         </div>
 
         {/* Dock - paling bawah di mobile */}
